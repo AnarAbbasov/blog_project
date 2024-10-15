a7c53fb2f562b103c4db83dd9a6fac397dbfc597
Anar Abbasov <anar_abbasov@hotmail.com>
2024-10-14 19:19:45 -0700

    additions

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+
 from blog import views
 from django.urls import path
 
@@ -10,4 +10,7 @@ urlpatterns=[
      path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
      path('post/new',views.CreatePostView.as_view(),name='post_new'),
      path('post/<int:pk>/edit', views.PostUpdateView.as_view(), name='post_edit'),
+     path('post/<int:pk>/remove', views.PostDeleteView.as_view(), name='post_remove'),
+     path('drafts', views.DraftListView.as_view(), name='post_draft_list'),
+     path('post/<int:pk>/',views.add_comment_to_post,name='add_comment_to_post')
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 from datetime import timezone
-from django.shortcuts import render
+from django.utils import timezone
+from django.shortcuts import render,get_object_or_404,redirect
 from django.views.generic import (
     TemplateView,ListView,
     DetailView, CreateView,
@@ -10,6 +11,7 @@ from blog.forms import Postform,CommentForm
 from blog.models import Post,Comment
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -21,7 +23,7 @@ class PostListView(ListView):
     model=Post
     
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now().order_by("-published_date"))
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by("-published_date")
 
 
 
@@ -45,4 +47,30 @@ class PostUpdateView(LoginRequiredMixin,UpdateView):
 
 class PostDeleteView(LoginRequiredMixin,DeleteView):
        model=Post
+       success_url=reverse_lazy('post_list')
+       
+class DraftListView(LoginRequiredMixin,ListView):
+      login_url='/login/'
+      redirect_field_name='blog/postlist.html'
+      model=Post
       
+      def get_queryset(self):
+           return Post.objects.filter(publisged_date__isnull=True).order_by('created_date')
+       
+       
+       
+       
+@login_required      
+def add_comment_to_post(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    if request.method=='POST':
+       form=CommentForm(request.POST)
+       if form.is_valid():
+          comment=form.save(commit=False)
+          comment.post=post
+          comment.save()
+          return redirect('post_detail',pk=post.pk)
+        
+    else:
+        form=CommentForm()
+        return render(request,'blog/comment_form.html',{'form':form})
